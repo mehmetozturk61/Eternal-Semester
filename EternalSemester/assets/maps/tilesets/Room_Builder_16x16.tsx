5e3a0ec58947661f4b3484018a96e25cf376c8c6
Alper Koçyiğit <145290229+alperkocyigit28@users.noreply.github.com>
2024-05-11 15:17:38 +0300

    Add files via upload

---
+++ EternalSemester/assets/maps/tilesets/Room_Builder_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Room_Builder_16x16" tilewidth="16" tileheight="16" tilecount="8284" columns="76">
+ <image source="../../moderninteriors-win/1_Interiors/16x16/Room_Builder_16x16.png" trans="ff00ff" width="1216" height="1744"/>
+</tileset>
